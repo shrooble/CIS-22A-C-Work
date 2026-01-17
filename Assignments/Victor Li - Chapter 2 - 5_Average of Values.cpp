dc6142bdef58b781ea27c287f9118ab6facf0797
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-16 22:55:25 -0800

    Update Victor Li - Chapter 2 - 5_Average of Values.cpp

--- Assignments/Victor Li - Chapter 2 - 5_Average of Values.cpp
+++ Assignments/Victor Li - Chapter 2 - 5_Average of Values.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
-  Chapter 2 Average of Values
-
+  Chapter 2: Average of Values
+  This program calculates and outputs the sum and average of five numbers
 */
 
 #include <iostream>
@@ -10,7 +10,13 @@ using namespace std;
 
 int main()
 {
+  double numOne = 28, numTwo = 32, numThree = 37, numFour = 24, numFive = 33, sum, average;
 
+  sum = numOne + numTwo + numThree + numFour + numFive;
+  average = sum / 5;
+
+  cout << "The sum of the five numbers are " << sum << "." << endl;
+  cout << "The average of the five numbers are " << average << "." << endl;
 
   return 0;
 }
