db62c65f3e4d68d7c120f4a604ac013569f50d44
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-17 10:27:26 -0800

    Create Victor Li - Chapter 2 - 15_Triangle Pattern.cpp

---
+++ Assignments/Victor Li - Chapter 2 - 15_Triangle Pattern.cpp
@@ -0,0 +1,16 @@
+/*
+  Victor Li
+  Chapter 2: Triangle Pattern
+  This program outputs a triangle pattern into the console
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+  cout << "   *\n" << "  ***\n" << " *****\n" << "*******\n";
+
+  return 0;
+}
