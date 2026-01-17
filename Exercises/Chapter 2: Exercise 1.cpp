5c24e05dc7a616fe71d7c1aefc916682c860b788
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-16 20:58:54 -0800

    Update Chapter 2: Exercise 1.cpp

--- Exercises/Chapter 2: Exercise 1.cpp
+++ Exercises/Chapter 2: Exercise 1.cpp
@@ -1,7 +1,7 @@
 /*  
     Victor Li 
     Chapter 2: Exercise 1
-    This program assigns and outputs the company name, hours worked, rate per hour, and gross pay to the console
+    This program assigns and outputs the company name, hours worked, rate per hour, and gross pay variables to the console
 */ 
 
 #include <iostream>
