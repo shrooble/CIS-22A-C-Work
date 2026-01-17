0cd9e0e551b33d4ba471a6ac1ef88e34f18a78e5
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-16 21:16:13 -0800

    Create Chapter 2: Sum of Two Numbers.cpp

---
+++ Assignments/Chapter 2: Sum of Two Numbers.cpp
@@ -0,0 +1,20 @@
+/*
+  Victor Li
+  Chapter 2: Sum of Two Numbers
+  This program assigns 50 and 100 to int variables, stores the sum of both in the total variable, and outputs the sum to the console
+*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+  int numberOne = 50, numberTwo = 100, total;
+
+// This calculates the sum of the 2 numbers
+  total = numberOne + numberTwo;
+  
+  cout << "The total value of " << numberOne << " and " << numberTwo << " is " << total << "." << endl;
+
+  return 0;
+}
