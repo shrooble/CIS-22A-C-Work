0de476c11c995896c2da4e9aa0199204747bcd7d
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-22 00:38:36 -0800

    Update Victor Li - Chapter 2 - 4_Restaurant Bill.cpp

--- Assignments/Chapter 2/Victor Li - Chapter 2 - 4_Restaurant Bill.cpp
+++ Assignments/Chapter 2/Victor Li - Chapter 2 - 4_Restaurant Bill.cpp
@@ -12,17 +12,17 @@ int main()
 {
   double mealCost = 88.67, taxPercent = 6.75, tipPercent = 20, taxAmount, totalTax, tipAmount, totalBill;
 
-// This calculates the cost of the meal by calculating the amount after tax and adding it to the subtotal to get totalTax
+// This calculates the cost of the meal by calculating the amount after tax and adding it to the subtotal to get the amount including tax
   taxAmount = mealCost * taxPercent;
   totalTax = mealCost + taxAmount;
   
-// This calculates the tip of the meal and adding it to totalTax to get the total bill of the meal
+// This calculates the tip of the meal and adds it to totalTax to get the total bill of the meal
   tipAmount = totalTax * tipPercent;
   totalBill = tipAmount + totalTax;
 
-  cout << "The subtotal is $" << mealCost << "." << endl;
-  cout << "The amount after tax is $" << totalTax << "." << endl;
-  cout << "The total bill after tax and including tip is $" << totalBill << "." << endl;
+  cout << "The subtotal is $" << mealCost << ".\n";
+  cout << "The amount after tax is $" << totalTax << ".\n";
+  cout << "The total bill after tax and including tip is $" << totalBill << ".\n";
 
   return 0;
 }
