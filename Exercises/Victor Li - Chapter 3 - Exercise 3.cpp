2c0ebd5a68b6084fa77f2e95e0d089cbc308f817
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-22 00:08:10 -0800

    Create Victor Li - Chapter 3 - Exercise 3.cpp

---
+++ Exercises/Victor Li - Chapter 3 - Exercise 3.cpp
@@ -0,0 +1,24 @@
+/*
+  Victor Li
+  Chapter 3: Exercise 3
+  
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  const double tax;
+  int hours, rate, grossPay;
+
+  cout << "Please enter your College or Company name\n";
+
+  getline(cin, );
+
+  
+
+  return 0;
+}
