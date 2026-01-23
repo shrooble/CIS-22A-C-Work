6545e48efbda72eb8e11392ea443f8ad4de6fb7a
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-22 22:10:03 -0800

    Update Victor Li - Chapter 2 - 8_Total Purchase.cpp

--- Assignments/Chapter 2/Victor Li - Chapter 2 - 8_Total Purchase.cpp
+++ Assignments/Chapter 2/Victor Li - Chapter 2 - 8_Total Purchase.cpp
@@ -1,31 +1,33 @@
 /*
   Victor Li
   Chapter 2: Total Purchase
-  This program calculates and outputs the subtotal, sales tax, and total of five items
+  This program calculates and outputs the subtotal, sales tax, and total purchase amount of five items
 */
 
 #include <iostream>
+#include <iomanip>
 
 using namespace std;
 
 int main()
 {
-  double itemOne = 15.95, itemTwo = 24.95, itemThree = 6.95, itemFour = 12.95, itemFive = 3.95, salesTax = 7, taxAmount, totalTax, subTotal;
+  double itemOne = 15.95, itemTwo = 24.95, itemThree = 6.95, itemFour = 12.95, itemFive = 3.95, salesTax = 0.07, taxAmount, totalAmount, subTotal;
 
-// This adds up all five items and calculates the sales tax before adding it with the original subtotal to get the total sale
+// This adds up all five items and calculates the sales tax before adding it to the subtotal to get the total amount
   subTotal = itemOne + itemTwo + itemThree + itemFour + itemFive;
   taxAmount = subTotal * salesTax;
-  totalTax = subTotal + taxAmount;
+  totalAmount = subTotal + taxAmount;
 
-  cout << "The price of item 1 is $" << itemOne << "." << endl;
-  cout << "The price of item 2 is $" << itemTwo << "." << endl;
-  cout << "The price of item 3 is $" << itemThree << "." << endl;
-  cout << "The price of item 4 is $" << itemFour << "." << endl;
-  cout << "The price of item 5 is $" << itemFive << "." << endl;
+  cout << "The price of item 1 is: $" << itemOne << endl;
+  cout << "The price of item 2 is: $" << itemTwo << endl;
+  cout << "The price of item 3 is: $" << itemThree << endl;
+  cout << "The price of item 4 is: $" << itemFour << endl;
+  cout << "The price of item 5 is: $" << itemFive << endl;
+  cout << "------------------------------\n";
 
-  cout << "The subtotal of the sale before tax is $" << subTotal << "." << endl;
-  cout << "The sales tax is $" << taxAmount << "." << endl;
-  cout << "The total is $" << totalTax << "." << endl;
+  cout << "The subtotal of the sale before tax is: $" << subTotal << endl;
+  cout << "The sales tax is: $" << fixed << setprecision(2) << taxAmount << endl;
+  cout << "The total is: $" << fixed << setprecision(2) << totalAmount << endl;
   
   return 0;
 }
