fd8f74850d958da3aad376c159036eb208b1076c
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-26 01:06:25 -0800

    Create Victor Li - Chapter 3 - 2_Stadium Seating.cpp

---
+++ Assignments/Chapter 3/Victor Li - Chapter 3 - 2_Stadium Seating.cpp
@@ -0,0 +1,26 @@
+/*
+  Victor Li
+  Chapter 3: 2 - Stadium Seating
+
+*/
+
+#include <iostream>
+#include <iomanip>
+
+using namespace std;
+
+int main()
+{
+  int classA = 15, classB = 12, classC = 9, ticketA, ticketB, ticketC, incomeGenerated;
+  
+  cout << "How many tickets of class A, B, or C tickets were sold?\n";
+  cout << "Enter in order of A B C with each input separated, enter 0 if there were no tickets sold:\n";
+  cin >> ticketA >> ticketB >> ticketC;
+
+// This
+  incomeGenerated = (ticketA * classA) + (ticketB * classB) + (ticketC * classC);
+
+  cout << "The income generated from ticket sales is: $" << fixed << setprecision(2) << incomeGenerated << endl;
+  
+  return 0;
+}
