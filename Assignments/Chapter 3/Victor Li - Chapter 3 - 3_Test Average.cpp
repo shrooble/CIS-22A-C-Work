98df06a7269e712f77b3f51b5bced8067c694d5b
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-27 14:36:49 -0800

    Update Victor Li - Chapter 3 - 3_Test Average.cpp

--- Assignments/Chapter 3/Victor Li - Chapter 3 - 3_Test Average.cpp
+++ Assignments/Chapter 3/Victor Li - Chapter 3 - 3_Test Average.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
-  Chapter 3: 2 - Stadium Seating
-
+  Chapter 3: 3 - Test Average
+  This program determines the average test score of 5 user inputted test scores
 */
 
 #include <iostream>
@@ -13,10 +13,10 @@ int main()
 {
   double scoreOne, scoreTwo, scoreThree, scoreFour, scoreFive, averageScore;
 
-  cout << "Enter 5 test scores with a space separation:\n";
+  cout << "Enter 5 test scores with a space separation between each input:\n";
   cin >> scoreOne << scoreTwo << scoreThree << scoreFour << scoreFive;
 
-// This
+// This calculates the average test score by getting the sum of the five test scores before dividing it by 5
   averageScore = (scoreOne + scoreTwo + scoreThree + scoreFour + scoreFive) / 5;
 
   cout << "The average test score is: " << fixed << setprecision(1) << averageScore << endl;
