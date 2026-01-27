d51332458c694efe91f1b374324dcb572a2e644c
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-27 15:15:48 -0800

    Update Victor Li - Chapter 3 - 5_Male and Female Percentages.cpp

--- Assignments/Chapter 3/Victor Li - Chapter 3 - 5_Male and Female Percentages.cpp
+++ Assignments/Chapter 3/Victor Li - Chapter 3 - 5_Male and Female Percentages.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
   Chapter 3: 5 - Male and Female Precentages
-
+  This program determines the percentage of male and female students in a class
 */
 
 #include <iostream>
@@ -10,8 +10,21 @@
 using namespace std;
 
 int main() {
+  int male, female, total;
+  double malePercentage, femalePercentage;
 
+  cout << "Enter the number of males and females in the class, the inputs should be separated by a space:\n";
+  cin >> male >> female;
 
+// This adds the number of male and female students to get the total number of students in a class
+  total = male + female;
 
+// This calculates the percentages for male and female students by dividing the male/female students by the number of total students respectfully
+  malePercentage = male / total;
+  femalePercentage = female / total;
+
+  cout << "The percentage of males in the class is: " << fixed << setprecision(2) << showpoint << malePercentage << "%\n";
+  cout << "The percentage of females in the class is: " << femalePercentage << "%\n";
+  
   return 0;
 }
