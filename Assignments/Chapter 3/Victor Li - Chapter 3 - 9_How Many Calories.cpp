73e0266da74562283ce78c42202e48c3b6d7fdca
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-27 23:29:50 -0800

    Create Victor Li - Chapter 3 - 9_How Many Calories.cpp

---
+++ Assignments/Chapter 3/Victor Li - Chapter 3 - 9_How Many Calories.cpp
@@ -0,0 +1,21 @@
+/*
+  Victor Li
+  Chapter 3: 9 - How Many Calories?
+
+*/
+
+#include <iostream>
+
+using namespace std;
+
+int main() {
+  int cookieBag = 30, servingAmount = 10, servingCalories = 300;
+  double cookieAmount;
+
+  cout << "How many cookies did you eat:\n";
+  cin >> cookieAmount;
+
+  
+
+  return 0;
+}
