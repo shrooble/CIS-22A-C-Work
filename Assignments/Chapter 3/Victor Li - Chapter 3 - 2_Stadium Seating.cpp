1bd96a11bd1894809aba15d2ac1920e6fa848bea
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-29 18:26:23 -0800

    Update Victor Li - Chapter 3 - 2_Stadium Seating.cpp

--- Assignments/Chapter 3/Victor Li - Chapter 3 - 2_Stadium Seating.cpp
+++ Assignments/Chapter 3/Victor Li - Chapter 3 - 2_Stadium Seating.cpp
@@ -1,5 +1,7 @@
 /*
   Victor Li
+  Team Name: Cat++
+  Team Members: Kenny Nguyen
   Chapter 3: 2 - Stadium Seating
   This program calculates the income generated from ticket sales
 */
@@ -11,16 +13,17 @@ using namespace std;
 
 int main()
 {
-  int classA = 15, classB = 12, classC = 9, ticketA, ticketB, ticketC, incomeGenerated;
+  int classA = 15, classB = 12, classC = 9, ticketA, ticketB, ticketC;
+  double incomeGenerated;
   
   cout << "How many tickets of class A, B, or C tickets were sold?\n";
   cout << "Enter in order of A B C with each input separated by a space, enter 0 if there were no tickets sold:\n";
   cin >> ticketA >> ticketB >> ticketC;
 
-// This calculates the income generated from each ticket class by their specific ticket class costs
+// This calculates the income generated from each ticket class by multiplying with their specific ticket class costs
   incomeGenerated = (ticketA * classA) + (ticketB * classB) + (ticketC * classC);
 
-  cout << "The income generated from ticket sales is: $" << fixed << setprecision(2) << showpoint << incomeGenerated << endl;
+  cout << "\nThe income generated from ticket sales is: $" << fixed << setprecision(2) << showpoint << incomeGenerated << endl;
   
   return 0;
 }
