9f57bf19388a71ef0c1d82d3f2e00dbd931d21d3
Victor <113949103+shrooble@users.noreply.github.com>
2026-01-29 18:13:46 -0800

    Update Victor Li - Chapter 3 - 1_Miles Per Gallon.cpp

--- Assignments/Chapter 3/Victor Li - Chapter 3 - 1_Miles Per Gallon.cpp
+++ Assignments/Chapter 3/Victor Li - Chapter 3 - 1_Miles Per Gallon.cpp
@@ -1,7 +1,7 @@
 /*
   Victor Li
   Chapter 3: 1 - Miles Per Gallon
-
+  This program calculates the miles per gallon of a car based on the car's maximum miles and gallons 
 */
 
 #include <iostream>
@@ -17,10 +17,10 @@ int main()
   cout << "Enter the number of miles the car can drive on a full tank.\n";
   cin >> milesPerTank;
 
-// This calculates the miles the car can go on a single gallon by dividing the miles the car can drive on a full tank by the gallons of gas the car can hold
+// This calculates the miles the car can go on a single gallon by dividing the miles the car can drive on a full tank, by the gallons of gas the car can hold
   milesPerGallon = milesPerTank / gallons;
 
-  cout << "The car can drive: " << 
+  cout << "The car can drive: " << milesPerGallon << " Miles Per Gallon\n";
 
   return 0;
 }
